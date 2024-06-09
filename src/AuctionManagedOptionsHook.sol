76620e84eb73f241fd60217b57b2ee39d3eb3539
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2024-06-09 21:01:16 +0800

    Implement funding and minting and burning options

--- src/AuctionManagedOptionsHook.sol
+++ src/AuctionManagedOptionsHook.sol
@@ -6,12 +6,14 @@ import {console2} from "forge-std/console2.sol";
 import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
 import {CurrencySettleTake} from "v4-core/libraries/CurrencySettleTake.sol"; // TODO: Use test/utils/CurrencySettler.sol instead?
 import {BaseHook} from "v4-periphery/BaseHook.sol";
+import {LiquidityAmounts} from "v4-periphery/libraries/LiquidityAmounts.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
-import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {BalanceDelta, toBalanceDelta} from "v4-core/types/BalanceDelta.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
 
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 
@@ -28,6 +30,7 @@ contract AuctionManagedOptionsHook is BaseHook {
 
     error CannotDepositDirectly();
     error NotEnoughDeposit();
+    error OnlyManager();
     error SwapFeeNotZero();
 
     struct PoolParams {
@@ -35,10 +38,11 @@ contract AuctionManagedOptionsHook is BaseHook {
         int24 tickUpper;
     }
 
-    struct ModifyLiquidityCallbackData {
+    struct CallbackData {
         PoolKey key;
         address sender;
         int256 liquidityDelta;
+        int256 delta0; // Needed so user can pay notional value when minting option
     }
 
     uint256 public constant MIN_DEPOSIT_PERIOD = 300;
@@ -47,6 +51,11 @@ contract AuctionManagedOptionsHook is BaseHook {
     mapping(PoolId => address) public managers;
     mapping(PoolId => uint256) public rents;
     mapping(PoolId => uint256) public lastRentChargeTimestamps;
+
+    mapping(PoolId => uint256) public currentFunding;
+    mapping(PoolId => uint256) public cumulativeFunding;
+    mapping(PoolId => uint256) public lastCumulativeFundingSnapshot;
+    mapping(PoolId => mapping(address => uint256)) public cumulativeFundingAtLastCharge;
 
     // TODO: Rename to balanceOf?
     // TODO: Add events on transfer/mint etc
@@ -198,9 +207,8 @@ contract AuctionManagedOptionsHook is BaseHook {
     }
 
     function modifyLiquidity(PoolKey memory key, int256 liquidityDelta) public payable returns (BalanceDelta delta) {
-        delta = abi.decode(
-            poolManager.unlock(abi.encode(ModifyLiquidityCallbackData(key, msg.sender, liquidityDelta))), (BalanceDelta)
-        );
+        delta =
+            abi.decode(poolManager.unlock(abi.encode(CallbackData(key, msg.sender, liquidityDelta, 0))), (BalanceDelta));
 
         // Make sure not negative
         liquidities[key.toId()][msg.sender] = liquidities[key.toId()][msg.sender] + liquidityDelta;
@@ -213,7 +221,7 @@ contract AuctionManagedOptionsHook is BaseHook {
 
     function unlockCallback(bytes calldata rawData) external override returns (bytes memory) {
         require(msg.sender == address(poolManager));
-        ModifyLiquidityCallbackData memory data = abi.decode(rawData, (ModifyLiquidityCallbackData));
+        CallbackData memory data = abi.decode(rawData, (CallbackData));
         PoolId poolId = data.key.toId();
 
         PoolParams memory pp = poolParams[poolId];
@@ -225,13 +233,72 @@ contract AuctionManagedOptionsHook is BaseHook {
         });
 
         (BalanceDelta delta,) = poolManager.modifyLiquidity(data.key, params, "");
-        int256 delta0 = delta.amount0();
+        int256 delta0 = delta.amount0() + data.delta0;
         int256 delta1 = delta.amount1();
 
         if (delta0 < 0) data.key.currency0.settle(poolManager, data.sender, uint256(-delta0), false);
         if (delta1 < 0) data.key.currency1.settle(poolManager, data.sender, uint256(-delta1), false);
         if (delta0 > 0) data.key.currency0.take(poolManager, data.sender, uint256(delta0), false);
         if (delta1 > 0) data.key.currency1.take(poolManager, data.sender, uint256(delta1), false);
-        return abi.encode(delta);
+        return abi.encode(toBalanceDelta(int128(delta0), int128(delta1)));
+    }
+
+    function setFunding(PoolKey calldata key, uint256 _funding) external {
+        PoolId poolId = key.toId();
+        if (msg.sender != managers[poolId]) {
+            revert OnlyManager();
+        }
+        _updateFunding(key, _funding);
+    }
+
+    function _updateFunding(PoolKey calldata key, uint256 _funding) internal {
+        PoolId poolId = key.toId();
+        cumulativeFunding[poolId] = calcCumulativeFunding(key);
+        lastCumulativeFundingSnapshot[poolId] = block.timestamp;
+
+        // TODO: Override if utilization ratio too high
+        currentFunding[poolId] = _funding;
+    }
+
+    function calcCumulativeFunding(PoolKey calldata key) public view returns (uint256) {
+        PoolId poolId = key.toId();
+        return cumulativeFunding[poolId]
+            + (block.timestamp - lastCumulativeFundingSnapshot[poolId]) * currentFunding[poolId];
+    }
+
+    function _chargeFunding(PoolKey calldata key, address user) internal {
+        PoolId poolId = key.toId();
+        uint256 funding = calcCumulativeFunding(key) - cumulativeFundingAtLastCharge[poolId][user];
+        uint256 fee = funding * uint256(liquidities[poolId][user]);
+        balances[poolId][user] -= fee;
+        cumulativeFundingAtLastCharge[poolId][user] = calcCumulativeFunding(key);
+    }
+
+    function mintCallOption(PoolKey calldata key, uint128 amount) external returns (BalanceDelta delta) {
+        _chargeFunding(key, msg.sender);
+        uint256 notional = _calcNotional(key, amount);
+        delta = abi.decode(
+            poolManager.unlock(abi.encode(CallbackData(key, address(this), -int128(amount), -int256(notional)))),
+            (BalanceDelta)
+        );
+    }
+
+    function burnCallOption(PoolKey calldata key, uint128 amount) external returns (BalanceDelta delta) {
+        _chargeFunding(key, msg.sender);
+        uint256 notional = _calcNotional(key, amount);
+        delta = abi.decode(
+            poolManager.unlock(abi.encode(CallbackData(key, address(this), int128(amount), int256(notional)))),
+            (BalanceDelta)
+        );
+    }
+
+    /// @notice Calculate notional value of option, i.e. the amount of currency0 that user
+    /// will receive if they exercise the option.
+    function _calcNotional(PoolKey calldata key, uint128 amount) internal view returns (uint256) {
+        return LiquidityAmounts.getAmount0ForLiquidity(
+            TickMath.getSqrtPriceAtTick(poolParams[key.toId()].tickLower),
+            TickMath.getSqrtPriceAtTick(poolParams[key.toId()].tickUpper),
+            amount
+        );
     }
 }
